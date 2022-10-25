a269a5708ed9f1da57c46814373cf4a3e08416ac
Yadi-Medina <116450959+Yadi-Medina@users.noreply.github.com>
2022-10-25 17:26:14 -0500

    Add files via upload

---
+++ cuadernillo de ejercicios_yadira/ejercicio 5/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace EJERCICIO_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("EJERCICIO 5");
+            Console.WriteLine("Un estudiante tiene 6 materias con sus calificaciones del semestre, diseña un programa que le ayude a saber su calificación");
+            Console.Write("Considera que si obtiene 7.6 hasta 10 decir HAS APROBADO y de 0 hasta 7.5 decir HAS REPROBADO.");
+
+            double cal1, cal2, cal3, cal4, cal5, cal6, prom;
+            Console.WriteLine("");
+            Console.WriteLine("..............................................................................................");
+            {
+                Console.WriteLine("Ingresa la calificació de MATEMÁTICAS: ");
+                cal1 = double.Parse(Console.ReadLine());
+                Console.WriteLine("Ingresa la calificació de EXPRESIÓN ORAL Y ESCRITA: ");
+                cal2 = double.Parse(Console.ReadLine());
+                Console.WriteLine("Ingresa la calificació de GEOGRAFÍA: ");
+                cal3 = double.Parse(Console.ReadLine());
+                Console.WriteLine("Ingresa la calificació de INGLÉS: ");
+                cal4 = double.Parse(Console.ReadLine());
+                Console.WriteLine("Ingresa la calificació de BILOGÍA: ");
+                cal5 = double.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese la calificación de ARTES");
+                cal6 = double.Parse(Console.ReadLine());
+
+                prom = (cal1 + cal2 + cal3 + cal4 + cal5 + cal6) / 6;
+                Console.WriteLine("El promedio que obtuviste es de: " + prom);
+
+                if (prom >= 7.6 && prom <= 10)
+                {
+                    Console.WriteLine("HAS APROBADO");
+                }
+                if (prom >= 1 && prom <= 7.5)
+                {
+                    Console.WriteLine("HAS REPROBADO" + "\n");
+                }
+            }
+            Console.WriteLine("..............................................................................");
+            
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ cuadernillo de ejercicios_yadira/ejercicio 6/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace act6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Realizar un programa que dado un operario se conoce su sueldo y los años de antigüedad.");
+            Console.WriteLine("Realizar un programa que lea los datos de entrada e indicar:");
+            Console.WriteLine("Si el sueldo es  menor  o igual a $10000 y su antigüedad es mayor o igual a 8 años, otorgarle un aumento del 25 %, mostrar el sueldo a pagar.");
+            Console.WriteLine("Si el sueldo es mayor o igual a $10000 y su antigüedad es menor o igual a 7 años, otorgarle un aumento de 15 %.");
+            Console.WriteLine("Si el sueldo es menor o igual a 90000 mostrar el sueldo.");
+            Console.WriteLine(".............................................................................................................. \n ");
+
+            double sueldo, sueldoT, aumento, total;
+            int antiguedad;
+            Console.WriteLine("Ingresa el sueldo del empleado");
+            sueldo = double.Parse(Console.ReadLine());
+            if (sueldo<=10000)   
+            {
+                Console.WriteLine("Ingresa los años del trabajo");
+                antiguedad = int.Parse(Console.ReadLine());
+                if (antiguedad>=8)
+                {
+                    aumento = sueldo * 0.25;
+                    sueldoT = aumento;
+                    total = sueldo + sueldoT;
+                    Console.WriteLine("Aumento $ " + sueldoT);
+                    Console.WriteLine("Sueldo total $" + total);
+                }
+                else if (antiguedad<=7)
+                {
+                    aumento = 10000 * 0.15;
+                    sueldoT = aumento;
+                    total = sueldo + sueldoT;
+                    Console.WriteLine("Aumento $ " + sueldoT);
+                    Console.WriteLine("Sueldo total $" + total);
+                }
+            }
+            else if (sueldo<=9000)
+            {
+                Console.WriteLine("El sueldo a pagar es de : $ " + sueldo);
+            }
+            Console.ReadKey();
+        }
+    }
+}
